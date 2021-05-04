889e420a83f85c607036793d2d3b42a17df2dff8
Y <209334631@qq.com>
2021-05-04 22:51:15 +0800

    main

--- WordCount.cpp
+++ WordCount.cpp
@@ -1,7 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 int main()
 {
-    printf("Hello World\n");
-    printf("Hello World\n");
+    char ch[100];
+    int i = 0, num = 0;
+    gets(ch);
+    char *p = ch;
+    while (*p != '\0')
+    {
+        if (*p != ' ' && *(p + 1) == ' ')
+            num++;
+        if (*p != ' ' && *(p + 1) == '\0')
+            num++;
+        p++;
+    }
+    printf("%d\n", num);
+    system("pause");
     return 0;
 }
